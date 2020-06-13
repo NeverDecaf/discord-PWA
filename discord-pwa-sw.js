fa65a861e13a04957dd2a53b9797d6b4c720dd31
NeverDecaf <jtr9289@gmail.com>
2020-06-13 08:35:50 -0400

    update cache every run

--- discord-pwa-sw.js
+++ discord-pwa-sw.js
@@ -1,25 +1,35 @@
 var cacheName = 'discord-pwa';
 var filesToCache = [
-  './',
-  './index.html',
-  './css/style.css',
-  './js/main.js'
+    './',
+    './index.html',
+    './css/style.css',
+    './js/main.js',
+	'./images/'
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+function update(request) {
+    caches.open(cacheName).then(function (cache) {
+        fetch(request).then(function (response) {
+            cache.put(request, response.clone())
+        });
+    });
+}
+
+/* Serve cached content, fallback to network, update cache from network regardless. */
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+    e.waitUntil(update(e.request));
 });
