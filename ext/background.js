21c4052685d1d338e067daad00a672e62e665353
NeverDecaf <jtr9289@gmail.com>
2020-12-16 18:27:49 -0500

    fix for broken webRequest

--- ext/background.js
+++ ext/background.js
@@ -1,49 +1,48 @@
-const HEADERS_TO_STRIP_LOWERCASE = [
-    'x-frame-options'
-];
-var cacheName = 'discord-pwa';
+const RELAX_CSP = {
+    condition: {
+        urlFilter: "||discord.com"
+    },
+    action: {
+        type: "modifyHeaders",
+        responseHeaders: [{
+            "header": "content-security-policy",
+            "operation": "remove"
+            // "value": "script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://*;"
+        }]
+    },
+    id: 2,
+    priority: 2
+};
 var default_options = {
     "custom_css": "",
     "custom_js": "",
     "custom_title": "DISCORD",
     "relax_CSP_styles": false
 };
-var relax_CSP_styles = false;
 
 chrome.storage.local.get(default_options, function (items) {
-    relax_CSP_styles = items.relax_CSP_styles
+    if (items.relax_CSP_styles)
+        chrome.declarativeNetRequest.updateDynamicRules({
+            removeRuleIds: [2],
+            addRules: [RELAX_CSP]
+        })
 });
 chrome.storage.onChanged.addListener(
     (changes, areaName) => {
         if (areaName === 'local') {
             if ('relax_CSP_styles' in changes)
-                relax_CSP_styles = changes['relax_CSP_styles'].newValue;
+                if (changes['relax_CSP_styles'].newValue)
+                    chrome.declarativeNetRequest.updateDynamicRules({
+                        removeRuleIds: [2],
+                        addRules: [RELAX_CSP]
+                    })
+            else
+                chrome.declarativeNetRequest.updateDynamicRules({
+                    removeRuleIds: [2]
+                })
         }
     }
 )
-
-chrome.webRequest.onHeadersReceived.addListener(
-    (details) => {
-        if (relax_CSP_styles) {
-            for (var i = 0; i < details.responseHeaders.length; i++) {
-                if (details.responseHeaders[i].name.toLowerCase() === "content-security-policy") {
-                    let csp = details.responseHeaders[i].value;
-                    let header = csp.replace(/connect-src ([^;]+);/, "connect-src $1 https://*;");
-                    header = header.replace(/style-src ([^;]+);/, "style-src $1 https://*;");
-                    header = header.replace(/img-src ([^;]+);/, "img-src $1 https://*;");
-                    details.responseHeaders[i].value = header;
-                    break;
-                }
-            }
-        }
-        return {
-            responseHeaders: details.responseHeaders.filter(header =>
-                !HEADERS_TO_STRIP_LOWERCASE.includes(header.name.toLowerCase()))
-        }
-    }, {
-        urls: ['https://discord.com/*']
-    },
-    ['blocking', 'responseHeaders']);
 
 chrome.runtime.onConnect.addListener(function (port) {
     var wid = port.sender.tab.windowId;
@@ -66,7 +65,7 @@ chrome.runtime.onConnect.addListener(function (port) {
                     }, (e) => {
                         chrome.tabs.insertCSS(tid, {
                             code: request.payload,
-                            frameId: e.filter(el => el.parentFrameId == 0)[0].frameId
+                            frameId: e.filter(el => el.parentFrameId == 0)[0] ? e.filter(el => el.parentFrameId == 0)[0].frameId : 0
                         });
                     });
                     break;
@@ -86,11 +85,11 @@ chrome.runtime.onConnect.addListener(function (port) {
                         }, (e) => {
                             chrome.tabs.executeScript(tid, {
                                 code: "let style = document.createElement('style'); style.textContent = `" + items.custom_css + "`; document.head.append(style);",
-                                frameId: e.filter(el => el.parentFrameId == 0)[0].frameId
+                                frameId: e.filter(el => el.parentFrameId == 0)[0] ? e.filter(el => el.parentFrameId == 0)[0].frameId : 0
                             });
                             chrome.tabs.executeScript(tid, {
                                 code: items.custom_js,
-                                frameId: e.filter(el => el.parentFrameId == 0)[0].frameId
+                                frameId: e.filter(el => el.parentFrameId == 0)[0] ? e.filter(el => el.parentFrameId == 0)[0].frameId : 0
                             });
                         });
                     });
