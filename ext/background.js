0f1ba61ea58e97110010b5125400c12e164c493a
NeverDecaf <jtr9289@gmail.com>
2022-06-29 09:05:01 -0400

    finally fixed the drawAttention issue (on windows)

--- ext/background.js
+++ ext/background.js
@@ -70,11 +70,60 @@ function createCSPRule() {
         });
 }
 createCSPRule();
-var default_options = {
+const default_options = {
     custom_css: "",
     custom_js: "",
     custom_title: "Discord",
     relax_CSP_styles: false,
+};
+var lastDrawAttention = Date.now();
+const drawAttentionTimeout = 4000; // full sequence takes ~4s, perhaps exactly 4s (timeout can be set much shorter though)
+chrome.storage.local.get({ lastDrawAttention: Date.now() }, (opts) => {
+    lastDrawAttention = opts.lastDrawAttention;
+});
+chrome.windows.onRemoved.addListener((windowId) => {
+    chrome.storage.local.get(
+        { discordWindowId: chrome.windows.WINDOW_ID_NONE },
+        (opts) => {
+            if (windowId == opts.discordWindowId)
+                chrome.storage.local.set({
+                    discordWindowId: chrome.windows.WINDOW_ID_NONE,
+                });
+        }
+    );
+});
+chrome.windows.onFocusChanged.addListener((windowId) => {
+    chrome.storage.local.get(
+        { discordWindowId: chrome.windows.WINDOW_ID_NONE },
+        (opts) => {
+            if (
+                opts.discordWindowId != chrome.windows.WINDOW_ID_NONE &&
+                windowId == opts.discordWindowId
+            )
+                chrome.storage.local.set({ canFlash: true });
+        }
+    );
+});
+const tryDrawAttention = (windowId) => {
+    chrome.storage.local.set({ discordWindowId: windowId });
+    if (Date.now() - lastDrawAttention > drawAttentionTimeout) {
+        chrome.storage.local.get({ canFlash: true }, (opts) => {
+            if (opts.canFlash) {
+                lastDrawAttention = Date.now();
+                chrome.windows.update(
+                    windowId,
+                    {
+                        drawAttention: true,
+                    },
+                    () =>
+                        chrome.storage.local.set({
+                            canFlash: false,
+                            lastDrawAttention: lastDrawAttention,
+                        })
+                );
+            }
+        });
+    }
 };
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     switch (request.dest) {
@@ -92,20 +141,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                         : 0;
                     switch (request.type) {
                         case "drawAttention":
-                            chrome.windows.get(sender.tab.windowId, {}, (w) => {
-                                if (!w.focused)
-                                    chrome.windows.update(
-                                        w.id,
-                                        {
-                                            drawAttention: false,
-                                        },
-                                        () =>
-                                            chrome.windows.update(w.id, {
-                                                drawAttention:
-                                                    request.payload > 0,
-                                            })
-                                    );
-                            });
+                            if (request.payload > 0) {
+                                tryDrawAttention(sender.tab.windowId);
+                            }
                             break;
                         case "clientcss":
                             chrome.scripting.insertCSS({
